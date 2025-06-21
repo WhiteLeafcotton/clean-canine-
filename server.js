0e8c28f77a66a48da30647ebe3db10e3a24ed8c2
WWW <clarkkimberly415@gmail.com>
2025-06-21 18:49:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.post('/create-checkout-session', async (req, res) => {
 
 const PORT = process.env.PORT || 4242;
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
+
